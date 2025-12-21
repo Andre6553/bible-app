9efdb90769502162443b4094204c17a6d637dac3
Bible App Dev <dev@bibleapp.com>
2025-12-22 00:07:12 +0200

    Blog: Filter out slash commands from keyword extraction

--- src/services/blogService.js
+++ src/services/blogService.js
@@ -307,7 +307,7 @@ export const analyzeUserInterests = async (userId) => {
         if (Object.keys(foundTopics).length < 2) {
             const stopWords = new Set(['bible', 'verse', 'chapter', 'what', 'does', 'say', 'about', 'the', 'and', 'for', 'die', 'van', 'vanuit', 'oor', 'hoe', 'waar', 'wat', 'is', 'om', 'te', 'met', 'by', 'aan']);
             const words = allText.split(/[\s,?.!]+/)
-                .filter(w => w.length > 4 && !stopWords.has(w));
+                .filter(w => w.length > 4 && !stopWords.has(w) && !w.startsWith('/'));
 
             const wordCounts = {};
             words.forEach(w => wordCounts[w] = (wordCounts[w] || 0) + 1);
@@ -393,7 +393,7 @@ export const getSearchKeywords = async (userId) => {
         if (searches && searches.length > 0) {
             const allText = searches.map(s => s.query).join(' ').toUpperCase();
             words = [...new Set(allText.split(/[\s,?.!]+/)
-                .filter(w => w.length > 3 && !STOP_WORDS.has(w)))]
+                .filter(w => w.length > 3 && !STOP_WORDS.has(w) && !w.startsWith('/')))]
                 .slice(0, 50)
                 .map(w => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase());
         }
