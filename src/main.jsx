a854a23cc6fbfa9b6079bf3cf39c3b78628d7149
Bible App Dev <dev@bibleapp.com>
2026-01-01 13:19:59 +0200

    chore: cleanup deployment trackers & verify auto-deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-// Service Worker registration is handled by vite-plugin-pwa automatically
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
