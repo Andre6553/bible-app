3218a97fbc1ffb1e78438a66f61a3275a0e7fb30
Bible App Dev <dev@bibleapp.com>
2026-01-01 15:40:51 +0200

    chore: update service worker and sync

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6fc00345'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5l35dvcfbgo"
+    "revision": "0.bq91df0c5uk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
